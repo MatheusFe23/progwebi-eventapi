847405d0a4323a727d4dadbc965ecfd0c212f03f
João Marcos Martins <joaomarcosmartins217@gmail.com>
2025-04-25 14:13:54 -0300

    Verificação

--- src/main/java/br/ueg/progwebi/eventapi/repository/EventRepository.java
+++ src/main/java/br/ueg/progwebi/eventapi/repository/EventRepository.java
@@ -4,6 +4,9 @@ import br.ueg.progwebi.eventapi.model.Event;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+
+
 @Repository
 public interface EventRepository extends JpaRepository<Event, Long> {
+
 }
