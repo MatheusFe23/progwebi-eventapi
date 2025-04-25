0d74de07658943a482519061de968a8222333415
João Marcos Martins <joaomarcosmartins217@gmail.com>
2025-04-25 11:25:07 -0300

    Adicionar anotações nos atributos de Event

--- src/main/java/br/ueg/progwebi/eventapi/model/Evento.java
+++ src/main/java/br/ueg/progwebi/eventapi/model/Evento.java
@@ -1,9 +1,11 @@
 package br.ueg.progwebi.eventapi.model;
 
+import jakarta.persistence.*;
 import lombok.*;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 import java.time.LocalDate;
+
+import static jakarta.persistence.GenerationType.SEQUENCE;
 
 
 @Getter
@@ -12,15 +14,30 @@ import java.time.LocalDate;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
+@Entity
 public class Evento {
-
-
+    public static final String SEQUENCE_NAME = "EVENT_ID_GENERATE";
+    @Id
+    @Column(name = "identificador")
+    @SequenceGenerator(
+            name = SEQUENCE_NAME,
+            sequenceName = SEQUENCE_NAME + "_bd",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = SEQUENCE,
+            generator = SEQUENCE_NAME
+    )
     private Long id;
+
+    @Column(name = "Nome", length = 50, nullable = false)
     private String nome;
+    @Column(name = "Descrição", length = 200, nullable = false)
     private String descricao;
     private LocalDate dataInicio;
     private LocalDate dataFim;
     private Boolean gratuito;
+    @Column(name = "Local", length = 100, nullable = false)
     private String local;
 
 }
