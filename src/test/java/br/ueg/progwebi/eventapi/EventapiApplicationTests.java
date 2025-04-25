560e44a65a01add2e226e06f9c1bea37786eee62
MatheusFe23 <155886669+MatheusFe23@users.noreply.github.com>
2025-04-25 09:50:53 -0300

    Add files via upload

---
+++ src/test/java/br/ueg/progwebi/eventapi/EventapiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ueg.progwebi.eventapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
